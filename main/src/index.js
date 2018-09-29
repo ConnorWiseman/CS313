de22f054938c28c6eaf146948333250263b1429c
Connor <wiseman.connor@gmail.com>
2018-09-29 07:34:27 -0600

    duplicating PHP app setup

--- main/src/index.js
+++ main/src/index.js
@@ -1,7 +1,20 @@
-const http = require('http');
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const path    = require('path');
 
-const app = http.createServer(function(req, res) {
-  res.end('Hello, world!');
+const app = express();
+
+app.set('views', path.join(__dirname, '../templates/views'));
+app.engine('html', exphbs({
+  defaultLayout: 'default',
+  extname:       '.html',
+  layoutsDir:    path.join(__dirname, '../templates/layouts'),
+  partialsDir:   path.join(__dirname, '../templates/partials')
+}));
+app.set('view engine', 'html');
+
+app.get('/', function(req, res) {
+  res.render('index');
 });
 
 app.listen(process.env.PORT);
