eb1cb032658b5a54796c1f940161d64d3a51b467
Connor <wiseman.connor@gmail.com>
2018-09-30 10:21:12 -0600

    Landing page

--- src/index.js
+++ src/index.js
@@ -5,16 +5,24 @@ const path    = require('path');
 const app = express();
 
 app.set('views', path.join(__dirname, '../templates/views'));
+
 app.engine('html', exphbs({
   defaultLayout: 'default',
   extname:       '.html',
   layoutsDir:    path.join(__dirname, '../templates/layouts'),
   partialsDir:   path.join(__dirname, '../templates/partials')
 }));
+
 app.set('view engine', 'html');
 
+app.use('/assets', express.static(path.join(__dirname, '../assets')));
+
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {
+    appTitle:       'CS313 - Web Engineering II',
+    appDescription: 'Example Heroku applications and practical Node.js & PHP examples.',
+    appThemeColor:  '#22b222'
+  });
 });
 
-app.listen(process.env.PORT);
+app.listen(3000)//process.env.PORT);
